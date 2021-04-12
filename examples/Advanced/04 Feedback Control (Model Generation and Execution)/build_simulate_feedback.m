41323091f1475168251fcbca042abe3a8d128c5c
CEA Wismar <71314407+cea-wismar@users.noreply.github.com>
2021-04-12 13:28:59 +0200

    Add files via upload
    Feedback Control into advanced examples

---
+++ examples/Advanced/04 Feedback Control (Model Generation and Execution)/build_simulate_feedback.m
@@ -0,0 +1,29 @@
+function build_simulate_feedback(mode)
+
+% this function sets options for model_builder
+% preliminary collection of data for the examples
+% will be replaced by generic GUI version
+% Input Arg for Feedforward SES:
+%   mode = 0 | 1 --> without or with feedforward
+% creates a model without out-ports
+
+
+% general options
+mbOpts.backend = 'Simulink';     % or 'SimulinkI' | 'Dymola' | 'OpenModelica'
+mbOpts.systemName = 'ctrlSys';
+mbOpts.cleanModel = false;        % true (close system & delete files)|false 
+
+% SES options
+mbOpts.ses.file = 'Feedback.mat';
+mbOpts.ses.opts = {'feedforward'};
+mbOpts.ses.vals = {mode};
+
+% simulation options
+mbOpts.sim.solver = 'ode45';
+mbOpts.sim.stopTime = 50;
+mbOpts.sim.maxStep = 0.1;
+
+mbOpts.plot.showResults = false;
+
+modelBuilder(mbOpts);
+return
