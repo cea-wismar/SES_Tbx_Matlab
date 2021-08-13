20fdd884d019ca78c95771ce75af6e2d03ae9a20
tina <tina@machina>
2021-08-13 10:46:04 +0200

    CD

--- examples/Advanced/04 Feedback Control (Model Generation and Execution)/ec_feedforward.m
+++ examples/Advanced/04 Feedback Control (Model Generation and Execution)/ec_feedforward.m
@@ -75,10 +75,6 @@ for st = 1:length(modes)          % for both structure variants
         % call model builder
         [Sim_modelName] = moBuild(mbOpts,components,couplings);
         
-        % call model builder
-        %[Sim_modelName] = moBuild(mbOpts);
-        % for Simulink backend this is just the name
-
         % transfer model name
         simOpts.systemName = Sim_modelName;
         % call execution unit and get results
@@ -127,83 +123,4 @@ if ~exp_success
 end
 
 end
-
-%%***********************************************************************%%
-%%           General functions for EC - DO NOT EDIT                      %%
-%%***********************************************************************%%
-
-function newPES = ecGeneralprune(sesOpts)
-        % prune with current settings of SES vars
-        % load SES -> pruning
-        load(sesOpts.file); % results in an SES object in variable 'new'
-        newSES = new;       % copy to local variable
-        newPES = fpes(newSES); % incarnate (F)PES object; not perfect, but..
-        % ... caused by flattening being a class-method of class fpes
-        
-        % splice cell arrays together
-        % there must be a simpler solution ...
-        N = length(sesOpts.opts);
-        pruneOpts = cell(1,N);
-        for I=1:N
-            pruneOpts{I} = {sesOpts.opts{I}, sesOpts.vals{I}};
-        end
-        % end splice
-        newPES.prune(pruneOpts);
-end
-    
-function newFPES = ecGeneralflatten(PES)
-    newFPES = PES;
-    newFPES.flatten;
-end
-
-function [objects, couplings] = ecGeneralprepare(FPES)
-        % do preprocessing of FPES so moBuild can directly build
-        % get model data from FPES
-        [leafNodes, couplings, parameters, isValid] = FPES.getResults;
-        if ~isValid
-            disp('FPES is not valid!')
-            return
-        end
-        % collect each element of Da and Parameters in object
-        objects = struct('name', [ ], 'mb', [ ], 'parameters', [ ]);
-        for k=1:length(leafNodes)
-            % store name
-            objects(k).name = leafNodes{k};
-            fieldTemp={ };
-            contentTemp={ };
-            for l = 1:size(parameters,1)
-                if strcmp(parameters{l,1},leafNodes{k})
-                    switch parameters{l,2}
-                        % store type
-                        case 'mb'
-                            objects(k).mb = eval(parameters{l,3});
-                            % store parameters
-                        otherwise
-                            fieldTemp(end+1) = parameters(l,2);
-                            val = parameters(l,3);
-                            % strip extra ''
-                            if val{1}(1) == ''''
-                                val{1} = val{1}(2:end-1);
-                            end
-                            contentTemp(end+1) = val;
-                    end
-                end
-            end
-            objects(k).parameters = cell2struct(contentTemp,fieldTemp,2);
-        end
-       
-        % remove NONE elements from list; C.D. 23.09.2020
-        valid_objects= struct('name','','mb','','parameters',''); % create the structure to allow concatening
-        for k = 1 : length(objects)
-            object_name = objects(k).name;
-            if ~strcmp(object_name(1:4), 'NONE')                  % just add valid objects
-                valid_objects = [valid_objects,objects(k)];
-            end
-        end  
-        
-        objects=valid_objects(2:end);                             % remove the first dummy entry
-        %%%%%%%%%%%%%% end remove NONE elements from list
-end
-       
-  
 
