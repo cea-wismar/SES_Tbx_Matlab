914d98150c3789d0ce35106b84f8428047bda7ae
tina <tina@machina>
2021-08-13 14:35:53 +0200

    CD

--- examples/Advanced/01 Variant Subsystem (simple)/startFcn_VarSubSys_v2.m
+++ examples/Advanced/01 Variant Subsystem (simple)/startFcn_VarSubSys_v2.m
@@ -3,6 +3,7 @@ function startFcn_VarSubSys_v2(VSS_MODE)
 % which is derived by a certain variant in the SES. The basic idea of this
 % example is taken from the Simulink examples. You can find further 
 % information about it (link from 2015(Feb)02):
+% accessed 2021-08
 % http://de.mathworks.com/help/simulink/examples/variant-subsystems.html
 % Input Arg:
 %   VSS_MODE = 1 | 2
